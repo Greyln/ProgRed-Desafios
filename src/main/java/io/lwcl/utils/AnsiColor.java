b3757c53ca41effb07be21c645b0d1cf69685c71
Lucyy_ <138951685+Greyln@users.noreply.github.com>
2024-11-12 17:36:25 -0300

    Create AnsiColor.java

---
+++ src/main/java/io/lwcl/utils/AnsiColor.java
@@ -0,0 +1,53 @@
+package io.lwcl.utils;
+
+public class AnsiColor {
+
+    // Reset
+    public static final String RESET = "\033[0m";  // Reset color
+
+    // Text Colors
+    public static final String BLACK = "\033[30m";
+    public static final String RED = "\033[31m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String BLUE = "\033[34m";
+    public static final String MAGENTA = "\033[35m";
+    public static final String CYAN = "\033[36m";
+    public static final String WHITE = "\033[37m";
+
+    // Light Text Colors
+    public static final String LIGHT_BLACK = "\033[90m";
+    public static final String LIGHT_RED = "\033[91m";
+    public static final String LIGHT_GREEN = "\033[92m";
+    public static final String LIGHT_YELLOW = "\033[93m";
+    public static final String LIGHT_BLUE = "\033[94m";
+    public static final String LIGHT_MAGENTA = "\033[95m";
+    public static final String LIGHT_CYAN = "\033[96m";
+    public static final String LIGHT_WHITE = "\033[97m";
+
+    // Background Colors
+    public static final String BLACK_BG = "\033[40m";
+    public static final String RED_BG = "\033[41m";
+    public static final String GREEN_BG = "\033[42m";
+    public static final String YELLOW_BG = "\033[43m";
+    public static final String BLUE_BG = "\033[44m";
+    public static final String MAGENTA_BG = "\033[45m";
+    public static final String CYAN_BG = "\033[46m";
+    public static final String WHITE_BG = "\033[47m";
+
+    // Light Background Colors
+    public static final String LIGHT_BLACK_BG = "\033[100m";
+    public static final String LIGHT_RED_BG = "\033[101m";
+    public static final String LIGHT_GREEN_BG = "\033[102m";
+    public static final String LIGHT_YELLOW_BG = "\033[103m";
+    public static final String LIGHT_BLUE_BG = "\033[104m";
+    public static final String LIGHT_MAGENTA_BG = "\033[105m";
+    public static final String LIGHT_CYAN_BG = "\033[106m";
+    public static final String LIGHT_WHITE_BG = "\033[107m";
+
+
+    // Styles
+    public static final String BOLD = "\033[1m";
+    public static final String UNDERLINE = "\033[4m";
+    public static final String REVERSED = "\033[7m";
+}
