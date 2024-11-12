827718f388e91659fbfaaa17399fd457d7dc1078
Lucyy_ <138951685+Greyln@users.noreply.github.com>
2024-11-12 16:09:14 -0300

    Create TicTactoe.java

---
+++ src/main/java/io/lwcl/TicTactoe.java
@@ -0,0 +1,14 @@
+package io.lwcl;
+
+public class TicTactoe {
+
+    /* Desafío 5: Juego de Ta-Te-Ti
+        Crear un juego de Ta-Te-Ti para dos jugadores.
+        Para resolver este desafío, debemos crear un programa que:
+        1. Cree un tablero de juego 3x3.
+        2. Pida a dos jugadores que ingresen sus movimientos.
+        3. Verifique si un jugador ha ganado o si el juego es un empate.
+        4. Muestre el resultado al final del juego.
+     */
+
+}
